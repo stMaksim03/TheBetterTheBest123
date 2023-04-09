41346d3757539cef8938e0dd44e4b0afe7b31086
stMaksim03 <91483289+stMaksim03@users.noreply.github.com>
2023-04-09 20:59:03 +0300

    16
    09.04.2023 20:58

--- computercraft/computer/2/MonWrite.lua
+++ computercraft/computer/2/MonWrite.lua
@@ -1,9 +1,10 @@
+os.loadAPI("TerminalAPI.lua")
 local textOpt = {{1}}
 print("Enter the x.y position of the text")
 print("x:")
 textOpt[1][1] = tonumber(read())
 print("y:")
-table.insert(textOpt,tonumber(read()))
+table.insert(textOpt[1],tonumber(read()))
 print("Enter The Text")
 table.insert(textOpt,read())
 print("Enter The Scale")
